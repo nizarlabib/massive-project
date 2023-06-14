f0239f7d7f01e7e3929ab5779250fdddde3c06cc
Salma <20102163@ittelkom-pwt.ac.id>
2023-06-15 00:54:02 +0700

    updat revisi

---
+++ src/components/mitra/gabungmitra.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../../styles/styles.css'; 
+
+const gabungmitra = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default gabungmitra
---
+++ src/components/tentang/tentang.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '../../styles/styles.css'; 
+
+const tentang = () => {
+    return (
+      <div>
+        <div class="bg-image w-100 d-flex justify-content-center bg-wisata">
+          <div class="position-absolute text-center">
+              <div class="tentang-margin-1">
+                <p><h3 class="text-hero-wisata">VACALAM</h3></p>
+              </div>
+              <div class="tentang-margin-1">
+                <h1 class="text-hero-wisata">Vacation Lampung</h1>
+              </div>
+          </div>
+        </div>
+    </div>
+    )
+  }
+  
+  export default tentang
