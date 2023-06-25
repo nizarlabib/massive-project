3a74781f84bc2c0a52c106406823d711af5e4810
Salma <20102163@ittelkom-pwt.ac.id>
2023-06-25 23:18:00 +0700

    update wisata

--- src/components/wisata/herowisata.js
+++ src/components/wisata/herowisata.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import '../../styles/styles.css'; 
+
 
 const Herowisata = () => {
   return (
